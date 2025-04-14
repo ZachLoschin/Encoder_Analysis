f93390f6fdf14f039ae49b88c5cf1960a62312b7
Zachary Loschinskey <zachloschin@gmail.com>
2025-04-14 15:13:05 -0400

    Finished preprocessing for encoder tests
    Saves Cut/Uncut versions of SVD Feats, Key point Feats, Neural FRs, Neural PCs, First contacts, Last contacts, and tongue lengths for visualizations

--- MATLAB/Encoder_Preprocessing.m
+++ MATLAB/Encoder_Preprocessing.m
@@ -3,18 +3,6 @@
 % March 2025
 % Cleaning up preprocessing file for MC engagement analysis
 
-%% Notes for processing
-
-% obj.me for each element has 1xTimepoints of the trial. It looks like this
-% is sampled at the same rate as the video data, so 1/400. This could be
-% used to extract the trials from the video data features.
-
-% Bruh of course this has one more timepoint than the SVD figures fml
-% Looks like I will have to loop through everything and find which one has
-% a mismatch and go from there so that I don't shift the trials while
-% cutting them up
-
-
 %% Finding "Kinematic Modes"
 clear,clc
 
@@ -48,8 +36,8 @@ params.condition(end+1) = {'hit==1' };    % left to right         % right hits, 
 
 % Take this big window for z-scoring to baseline
 params.tmin = -2;
-params.tmax = 4;
-params.dt = 1/200;
+params.tmax = 5;
+params.dt = 1/100;
 pre_gc_points = -params.tmin / params.dt;
 
 % smooth with causal gaussian kernel
@@ -86,23 +74,37 @@ for sessix = 1:numel(meta)
 end
 
 %% Check if video features for SVD have same lengths in obj.me
-folderPath = 'C:\Research\Encoder_Modeling\TD13d\2024-11-12\cam1';
+folderPath = 'C:\Research\Encoder_Modeling\Encoder_Analysis\Data\video_data\TD13d\2024-11-12\cam1';
 [frameCounts, mismatchFlags] = checkVideoFrameCounts(folderPath, obj);
 
 
-%% Import the SVD feature matrix
-load('C:\Research\Encoder_Modeling\Video_Features\TD13d\2024-11-12\SVD_Features_Cam0_TD13d_2024_11_12.mat');
+%% Import the Facemap object and extract SVD features
+% load('C:\Research\Encoder_Modeling\Video_Features\TD13d\2024-11-12\SVD_Features_Cam0_TD13d_2024_11_12.mat');
+
+% Load facemap processed files
+SVD_feats_cam0_struct = load('C:\Research\Encoder_Modeling\Encoder_Analysis\Data\SVD_features\FaceMap_Processed\Cam0_TD13d_2024-11-12_cam_0_date_2024_11_12_time_17_49_00_v001_proc.mat');
+% SVD_feats_cam1_struct = load('C:\Research\Encoder_Modeling\Encoder_Analysis\Data\SVD_features\FaceMap_Processed\Cam1_TD13d_2024-11-12_cam_1_date_2024_11_12_time_17_49_00_v001_proc.mat');
+
+% Extract features
+SVD_Feats_cam0 = [SVD_feats_cam0_struct.motSVD_0(:,1:50), SVD_feats_cam0_struct.movSVD_0(:,1:50)];
+% SVD_Feats_cam1 = [SVD_feats_cam1_struct.motSVD_0(:,1:50), SVD_feats_cam1_struct.movSVD_0(:,1:50)];
+% SVD_features = [SVD_Feats_cam0, SVD_Feats_cam1];
+
+SVD_features = SVD_Feats_cam0;
+
+clear SVD_feats_cam0_struct
+% clear SVD_feats_cam1_struct
 
 
-%%
-% Get each trial of SVD_features using the framcounts
+%% Get each trial of SVD_features using the framcounts
+cam_framerate = 400;
 trialFeatures = cell(length(frameCounts), 1);
 t = [];
 startIdx = 1;
 for i = 1:length(frameCounts)
     endIdx = startIdx + frameCounts(i) - 1;
     trialFeatures{i} = SVD_features(startIdx:endIdx, :);
-    t = [t, size(trialFeatures{i}, 1) / 400];
+    t = [t, size(trialFeatures{i}, 1) / cam_framerate];
     startIdx = endIdx + 1;
 end
 
@@ -116,6 +118,7 @@ for sessix = 1:numel(meta)
     message = strcat('----Getting kinematic data for session',{' '},num2str(sessix), {' '},'out of',{' '},num2str(nSessions),'----');
     disp(message)
     kin(sessix) = getKinematics(obj(sessix), me(sessix), params(sessix));
+    pos = getKeypointsFromVideo(obj(sessix),params(sessix));
 end
 
 % clearvars -except kin meta obj params
@@ -129,8 +132,9 @@ sessix = 1;
 kinix =  find(strcmp(kin(sessix).featLeg,kinfeat));
 all_length = kin.dat(:,condtrix,kinix);
 
-% trim off first second because its before the go cue
-all_length2 = all_length(pre_gc_points-100+1:end, :);
+%% -- CHECK THAT THE CHOPPING PROCEDURE HAS THE RIGHT UNITS -- %%
+%  -- The contacts are relative to the GC correct? -- %
+%  -- Check that if this is true, chops are being handled correctly -- %
 
 %% -- Extract All Trial Lick Port Contacts -- %%
 all_contacts = obj.bp.ev.lickL;
@@ -148,13 +152,18 @@ R1_Trials = params.trialid{8};
 R4_Trials = params.trialid{9};
 
 % Filter Tongue Length
-% Remove first second of data because its before gc
-R1_Tongue = all_length(pre_gc_points-200+1:end, R1_Trials);  % 100 before gc to end of trial (600 points)
-R4_Tongue = all_length(pre_gc_points-200+1:end, R4_Trials);  % 100 before gc to end of trial (600 points)
+R1_Tongue = all_length(pre_gc_points-100+1:end, R1_Trials);  % -1s through 4s (500 points)
+R4_Tongue = all_length(pre_gc_points-100+1:end, R4_Trials);
 
 % Filter LP Contacts
 R1_Contacts = all_contacts(R1_Trials);  % these contacts are relative to the gc
 R4_Contacts = all_contacts(R4_Trials);  % these contacts are relative to the gc
+
+%% Find trial FCs, Last Relevant Contacts (LRCs), and Trials2Remove
+[trials2removeR1, FCs_R1, LRCs_R1] = filter_trials_by_licking(R1_Contacts, min_licks=3);
+[trials2removeR4, FCs_R4, LRCs_R4] = filter_trials_by_licking(R4_Contacts, min_licks=5);
+
+% This trials2remove are indices into R1 and R4_Contacts, not trial numbers
 
 %% Get SVD features by trial
 R1_Features = trialFeatures(R1_Trials);
@@ -167,36 +176,82 @@ gc = obj.bp.ev.goCue(:);
 R1_GC = gc(R1_Trials);
 R4_GC = gc(R4_Trials);
 
+vid_offset = 0.49;  % time axis offset btwn video and spike glx
 
-% Remove trials where the GC is past the end of the video
-[R1_Valid_Trials, R1_Valid_Features, R1_Valid_Times, R1_Valid_GC] = ...
-    filter_valid_trials(R1_Trials, R1_Features, R1_Times, R1_GC, 0.49);
+% Find invalid trials where the GC is after the end of the video
+[R1_Valid_Trials, ~, ~, ~] = ...
+    filter_valid_trials(R1_Trials, R1_Features, R1_Times, R1_GC, vid_offset);
 
-[R4_Valid_Trials, R4_Valid_Features, R4_Valid_Times, R4_Valid_GC] = ...
-    filter_valid_trials(R4_Trials, R4_Features, R4_Times, R4_GC, 0.49);
+[R4_Valid_Trials, ~, ~, ~] = ...
+    filter_valid_trials(R4_Trials, R4_Features, R4_Times, R4_GC, vid_offset);
 
-% Now pass the valid trials to the feature extraction function
-R1_Trial_Features = extract_trial_features(R1_Valid_Features, R1_Valid_Times, R1_Valid_GC, 399, 2000);
-R4_Trial_Features = extract_trial_features(R4_Valid_Features, R4_Valid_Times, R4_Valid_GC, 399, 2000);
+% These are also trial indices into RN_Trials, not trial numbers
+
+%% Filter the trials by valid from SVD analysis and removal from contacts
+final_R1_trials = setdiff(R1_Valid_Trials, trials2removeR1);
+final_R4_trials = setdiff(R4_Valid_Trials, trials2removeR4);
+
+FCs_R1_clean = FCs_R1(final_R1_trials);
+LRCs_R1_clean = LRCs_R1(final_R1_trials);
+
+FCs_R4_clean = FCs_R4(final_R4_trials);
+LRCs_R4_clean = LRCs_R4(final_R4_trials);
+
+R1_Valid_Features = R1_Features(final_R1_trials);
+R1_Valid_Times = R1_Times(final_R1_trials);
+R1_Valid_GC = R1_GC(final_R1_trials);
+
+R4_Valid_Features = R4_Features(final_R4_trials);
+R4_Valid_Times = R4_Times(final_R4_trials);
+R4_Valid_GC = R4_GC(final_R4_trials);
+
+%% Now pass the valid trials to the feature extraction function
+pregc_frames = (400 * 1) -1;
+postgc_frames = (400 * 5);
+R1_Trial_Features = extract_trial_features(R1_Valid_Features, R1_Valid_Times, R1_Valid_GC, pregc_frames, postgc_frames);
+R4_Trial_Features = extract_trial_features(R4_Valid_Features, R4_Valid_Times, R4_Valid_GC, pregc_frames, postgc_frames);
+
 
 %% Get into format for storage
 % Assuming R1_Trial_Features and R4_Trial_Features are cell arrays of trial matrices
-% Each matrix in the cell array has dimensions 2400 x 200 (timepoints x features)
+% Each matrix in the cell array has dimensions 2000 x 100 (timepoints x features)
 
 % Initialize empty arrays to store the reshaped features
-reshaped_R1_Trial_Features = [];
-reshaped_R4_Trial_Features = [];
+R1_SVD_Features_Uncut = [];
+R4_SVD_Features_Uncut = [];
 
 % Loop through the trials in R1_Trial_Features and R4_Trial_Features
 for i = 1:length(R1_Trial_Features)
-    % Reshape each trial matrix into (2400*trials) x 200 by concatenating across the time dimension
-    reshaped_R1_Trial_Features = [reshaped_R1_Trial_Features; R1_Trial_Features{i}];
+    % Reshape each trial matrix into (2400*trials) x 100 by concatenating across the time dimension
+    R1_SVD_Features_Uncut = [R1_SVD_Features_Uncut; R1_Trial_Features{i}];
 end
 
-for i =1:length(R4_Trial_Features)
-    reshaped_R4_Trial_Features = [reshaped_R4_Trial_Features; R4_Trial_Features{i}];
+for i = 1:length(R4_Trial_Features)
+    R4_SVD_Features_Uncut = [R4_SVD_Features_Uncut; R4_Trial_Features{i}];
 end
 
+% Convert cell array to 3D matrix
+R1_SVD_Features_mat = cat(3, R1_Trial_Features{:});
+R4_SVD_Features_mat = cat(3, R4_Trial_Features{:});
+
+R1_SVD_Features_Cut = chop_and_stack_neural_data(R1_SVD_Features_mat, LRCs_R1_clean);
+R4_SVD_Features_Cut = chop_and_stack_neural_data(R4_SVD_Features_mat, LRCs_R4_clean);
+
+
+%% Get the keypoints by trial
+R1_Keypoints_unfiltered = pos(101:end, :, R1_Trials);
+R4_Keypoints_unfiltered = pos(101:end, :, R4_Trials);
+
+R1_Keypoints = R1_Keypoints_unfiltered(:, :, final_R1_trials);
+R4_Keypoints = R4_Keypoints_unfiltered(:, :, final_R4_trials);
+
+% Reshape into matrix for storage
+R1_Keypoints_Uncut = reshape(permute(R1_Keypoints, [1, 3, 2]), [], size(R1_Keypoints, 2));
+R4_Keypoints_Uncut = reshape(permute(R4_Keypoints, [1, 3, 2]), [], size(R4_Keypoints, 2));
+
+% Chop trials to LRCs for Cut storage
+R1_Keypoints_Cut = chop_and_stack_neural_data(R1_Keypoints, LRCs_R1_clean);
+R4_Keypoints_Cut = chop_and_stack_neural_data(R4_Keypoints, LRCs_R4_clean);
 
 %% -- Get Region Specific Neural Data and Filter It-- %%
 % Tudor code for separating probe 1 and 2
@@ -208,26 +263,102 @@ probe1_trialdat = obj.trialdat(:,probe1, :);
 probe2_trialdat = obj.trialdat(:,probe2, :);
 
 % Limit to the desired trials
-probe1_R4 = probe1_trialdat(:,:,R4_Valid_Trials);  % -2 to 5s 200Hz
-probe2_R4 = probe2_trialdat(:,:,R4_Valid_Trials);
+% This is done in multiple rouds since the final_RN_trials are indices into
+% the RN_trials, not absolute trial numbers
+probe1_R4 = probe1_trialdat(:,:,R4_Trials);  % -2 to 4s 100Hz
+probe2_R4 = probe2_trialdat(:,:,R4_Trials);
 
-probe1_R1 = probe1_trialdat(:,:,R1_Valid_Trials);
-probe2_R1 = probe2_trialdat(:,:,R1_Valid_Trials);
+probe1_R4 = probe1_R4(:,:,final_R4_trials);
+probe2_R4 = probe2_R4(:,:,final_R4_trials);
 
-% %% Normalize the neural data to the baseline period
-probe1_R4 = zscore_pregc(probe1_R4, pre_gc_points);
-probe1_R1 = zscore_pregc(probe1_R1, pre_gc_points);
-probe2_R4 = zscore_pregc(probe2_R4, pre_gc_points);
-probe2_R1 = zscore_pregc(probe2_R1, pre_gc_points);
+probe1_R1 = probe1_trialdat(:,:,R1_Trials);
+probe2_R1 = probe2_trialdat(:,:,R1_Trials);
 
-%% Get into format for storage
+probe1_R1 = probe1_R1(:,:,final_R1_trials);
+probe2_R1 = probe2_R1(:,:,final_R1_trials);
+
+%% Normalize the neural data to the baseline period
+probe1_R4_norm = zscore_pregc(probe1_R4, pre_gc_points);  % -1 to 5s 100Hz
+probe1_R1_norm = zscore_pregc(probe1_R1, pre_gc_points);
+probe2_R4_norm = zscore_pregc(probe2_R4, pre_gc_points);
+probe2_R1_norm = zscore_pregc(probe2_R1, pre_gc_points);
+
+%% Get into uncut format for storage
 % timepoints x neurons x trials
 % puts into timepoines x trials x neurons and then reshapes
-concatenated_probe1_R4 = reshape(permute(probe1_R4, [1, 3, 2]), [], size(probe1_R4, 2));
-concatenated_probe1_R1 = reshape(permute(probe1_R1, [1, 3, 2]), [], size(probe1_R1, 2));
-concatenated_probe2_R4 = reshape(permute(probe2_R4, [1, 3, 2]), [], size(probe2_R4, 2));
-concatenated_probe2_R1 = reshape(permute(probe2_R1, [1, 3, 2]), [], size(probe2_R1, 2));
+probe1_R4_Uncut = reshape(permute(probe1_R4_norm, [1, 3, 2]), [], size(probe1_R4_norm, 2));
+probe1_R1_Uncut = reshape(permute(probe1_R1_norm, [1, 3, 2]), [], size(probe1_R1_norm, 2));
+probe2_R4_Uncut = reshape(permute(probe2_R4_norm, [1, 3, 2]), [], size(probe2_R4_norm, 2));
+probe2_R1_Uncut = reshape(permute(probe2_R1_norm, [1, 3, 2]), [], size(probe2_R1_norm, 2));
 
+%% Chop up the neural data to relevant trial ends
+% Handles LRCs relative to GC if neural data has 1s pre GC stored
+probe1_R4_Cut = chop_and_stack_neural_data(probe1_R4_norm, LRCs_R4_clean);
+probe1_R1_Cut = chop_and_stack_neural_data(probe1_R1_norm, LRCs_R1_clean);
+
+probe2_R4_Cut = chop_and_stack_neural_data(probe2_R4_norm, LRCs_R4_clean);
+probe2_R1_Cut = chop_and_stack_neural_data(probe2_R1_norm, LRCs_R1_clean);
+
+%% Neural PCs
+% Get -1s -> 5s data
+probe1 = probe1_trialdat(101:end, :, :);
+probe2 = probe2_trialdat(101:end, :, :);
+
+[num_timepoints1, ~, num_trials1] = size(probe1);
+[num_timepoints2, ~, num_trials2] = size(probe2);
+
+% Reshape to (timepoints x trials) x features
+probe1_PCA = reshape(permute(probe1, [1, 3, 2]), [], size(probe1, 2));
+probe2_PCA = reshape(permute(probe2, [1, 3, 2]), [], size(probe2, 2));
+
+% PCA on probe1
+num_PCs = 12;
+[coeff1, score1, latent1, tsquared1, explained1, mu1] = pca(probe1_PCA);
+score1 = score1(:, 1:num_PCs);
+score1_reshaped = reshape(score1, num_timepoints1, num_trials1, num_PCs);
+
+% PCA on probe2
+[coeff2, score2, latent2, tsquared2, explained2, mu2] = pca(probe2_PCA);
+score2 = score2(:, 1:num_PCs);
+score2_reshaped = reshape(score2, num_timepoints2, num_trials2, num_PCs);
+
+% Separate PCs by trial type and filter by valid trials
+Probe1_PCs_R1 = score1_reshaped(:, R1_Trials, :);
+Probe1_PCs_R4 = score1_reshaped(:, R4_Trials, :);
+Probe2_PCs_R1 = score2_reshaped(:, R1_Trials, :);
+Probe2_PCs_R4 = score2_reshaped(:, R4_Trials, :);
+
+Probe1_PCs_R1 = Probe1_PCs_R1(:, final_R1_trials, :);
+Probe1_PCs_R4 = Probe1_PCs_R4(:, final_R4_trials, :);
+Probe2_PCs_R1 = Probe2_PCs_R1(:, final_R1_trials, :);
+Probe2_PCs_R4 = Probe2_PCs_R4(:, final_R4_trials, :);
+
+% Reshape into matrices for Uncut storage
+Probe1_PCs_R1_Uncut = reshape(Probe1_PCs_R1, [], size(Probe1_PCs_R1, 3));
+Probe1_PCs_R4_Uncut = reshape(Probe1_PCs_R4, [], size(Probe1_PCs_R4, 3));
+Probe2_PCs_R1_Uncut = reshape(Probe2_PCs_R1, [], size(Probe2_PCs_R1, 3));
+Probe2_PCs_R4_Uncut = reshape(Probe2_PCs_R4, [], size(Probe2_PCs_R4, 3));
+
+% Reshape to input into chopping procedure
+Probe1_PCs_R4_Cut = chop_and_stack_neural_data(permute(Probe1_PCs_R4, [1, 3, 2]), LRCs_R4_clean);
+Probe2_PCs_R4_Cut = chop_and_stack_neural_data(permute(Probe2_PCs_R4, [1, 3, 2]), LRCs_R4_clean);
+Probe1_PCs_R1_Cut = chop_and_stack_neural_data(permute(Probe1_PCs_R1, [1, 3, 2]), LRCs_R1_clean);
+Probe2_PCs_R1_Cut = chop_and_stack_neural_data(permute(Probe2_PCs_R1, [1, 3, 2]), LRCs_R1_clean);
+
+%% Get the tongue length, FLCs, and LRCs times to save
+SR = 100;
+% Filter Tongue Length
+R1_Tongue = all_length(pre_gc_points-100+1:end, R1_Trials);  % -1s through 4s (500 points)
+R4_Tongue = all_length(pre_gc_points-100+1:end, R4_Trials);
+
+R1_Tongue_Uncut = R1_Tongue(:,final_R1_trials);
+R4_Tongue_Uncut = R4_Tongue(:,final_R4_trials);
+
+FCs_Adj_R1 = FCs_R1_clean + SR;
+FCs_Adj_R4 = FCs_R4_clean + SR;
+
+LRCs_Adj_R1 = LRCs_R1_clean + SR;
+LRCs_Adj_R4 = LRCs_R4_clean + SR;
 
 %% -- Construct the Output File -- %%
 sessionName = meta.anm;
@@ -243,17 +374,52 @@ if ~exist(outputFolder, 'dir')
     mkdir(outputFolder);
 end
 
-% Save R4 Files
-csvwrite(fullfile(outputFolder, "Probe1_R4.csv"), concatenated_probe1_R4);
-csvwrite(fullfile(outputFolder, "Probe2_R4.csv"), concatenated_probe2_R4);
-csvwrite(fullfile(outputFolder, "R4_Features.csv"), reshaped_R4_Trial_Features);
+% Save SVD Features
+csvwrite(fullfile(outputFolder, "SVD_Feats_R1_Uncut.csv"), R1_SVD_Features_Uncut);
+csvwrite(fullfile(outputFolder, "SVD_Feats_R4_Uncut.csv"), R4_SVD_Features_Uncut);
+csvwrite(fullfile(outputFolder, "SVD_Feats_R1_Cut.csv"), R1_SVD_Features_Cut);
+csvwrite(fullfile(outputFolder, "SVD_Feats_R4_Cut.csv"), R4_SVD_Features_Cut);
+
+% Save key point features
+csvwrite(fullfile(outputFolder, "Keypoint_Feats_R1_Uncut.csv"), R1_Keypoints_Uncut);
+csvwrite(fullfile(outputFolder, "Keypoint_Feats_R4_Uncut.csv"), R4_Keypoints_Uncut);
+csvwrite(fullfile(outputFolder, "Keypoint_Feats_R1_Cut.csv"), R1_Keypoints_Cut);
+csvwrite(fullfile(outputFolder, "Keypoint_Feats_R4_Cut.csv"), R4_Keypoints_Cut);
+
+% Save Neural FRs
+csvwrite(fullfile(outputFolder, "Probe1_R1_Uncut.csv"), probe1_R1_Uncut);
+csvwrite(fullfile(outputFolder, "Probe1_R4_Uncut.csv"), probe1_R4_Uncut);
+csvwrite(fullfile(outputFolder, "Probe1_R1_Cut.csv"), probe1_R1_Cut);
+csvwrite(fullfile(outputFolder, "Probe1_R4_Cut.csv"), probe1_R4_Cut);
 
 
-% Save R1 Files
-csvwrite(fullfile(outputFolder, "Probe1_R1.csv"), concatenated_probe1_R1);
-csvwrite(fullfile(outputFolder, "Probe2_R1.csv"), concatenated_probe2_R1);
-csvwrite(fullfile(outputFolder, "R1_Features.csv"), reshaped_R1_Trial_Features);
+csvwrite(fullfile(outputFolder, "Probe2_R1_Uncut.csv"), probe2_R1_Uncut);
+csvwrite(fullfile(outputFolder, "Probe2_R4_Uncut.csv"), probe2_R4_Uncut);
+csvwrite(fullfile(outputFolder, "Probe2_R1_Cut.csv"), probe2_R1_Cut);
+csvwrite(fullfile(outputFolder, "Probe2_R4_Cut.csv"), probe2_R4_Cut);
 
+% Save Neural PCs
+csvwrite(fullfile(outputFolder, "Probe1_R1_Uncut.csv"), Probe1_PCs_R1_Uncut);
+csvwrite(fullfile(outputFolder, "Probe1_R4_Uncut.csv"), Probe1_PCs_R4_Uncut);
+csvwrite(fullfile(outputFolder, "Probe1_R1_Cut.csv"), Probe1_PCs_R1_Cut);
+csvwrite(fullfile(outputFolder, "Probe1_R4_Cut.csv"), Probe1_PCs_R4_Cut);
+
+
+csvwrite(fullfile(outputFolder, "Probe2_R1_Uncut.csv"), Probe2_PCs_R1_Uncut);
+csvwrite(fullfile(outputFolder, "Probe2_R4_Uncut.csv"), Probe2_PCs_R4_Uncut);
+csvwrite(fullfile(outputFolder, "Probe2_R1_Cut.csv"), Probe2_PCs_R1_Cut);
+csvwrite(fullfile(outputFolder, "Probe2_R4_Cut.csv"), Probe2_PCs_R4_Cut);
+
+% Save Tongue Length for visualizations
+csvwrite(fullfile(outputFolder, "Tongue_R1.csv"), R1_Tongue_Uncut);
+csvwrite(fullfile(outputFolder, "Tongue_R4.csv"), R4_Tongue_Uncut);
+
+% Save FCs and LRCs
+csvwrite(fullfile(outputFolder, "FCs_R1.csv"), FCs_Adj_R1);
+csvwrite(fullfile(outputFolder, "FCs_R4.csv"), FCs_Adj_R4);
+
+csvwrite(fullfile(outputFolder, "LRCs_R1.csv"), LRCs_Adj_R1);
+csvwrite(fullfile(outputFolder, "LRCs_R1.csv"), LRCs_Adj_R1);
 
 % Save metadata as a .txt file for record-keeping
 metadataFile = fullfile(outputFolder, 'metadata.txt');
@@ -290,7 +456,7 @@ end
 
 
 function zscored_data = zscore_pregc(data, pre_gc_points)
-    data_pregc = data(1:800,:,:);
+    data_pregc = data(1:200,:,:);
     % Concatenate trials along the second dimension (time dimension)
     concatenated_data = reshape(data_pregc, [], size(data_pregc, 2));
     
@@ -302,6 +468,6 @@ function zscored_data = zscore_pregc(data, pre_gc_points)
     zscored_data = (data - neuron_means ./ ...
                         neuron_stds);
 
-    % chop the data to the gc to end
-    zscored_data = zscored_data(pre_gc_points-400+1:end, :, :);
+    % chop the data to the gc-1s to end
+    zscored_data = zscored_data(pre_gc_points-100+1:end, :, :);
 end
